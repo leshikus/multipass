6443db68ad9392c0145abce69847b679459d874d
Alexei Fedotov <alexei.fedotov@gmail.com>
2023-12-11 13:06:06 +0100

    Initial commit

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh -nv
-sh miniconda.sh -b -u -p $HOME/miniconda3
-
-conda install -y -n conda-smithy -c conda-forge conda-smithy
-
